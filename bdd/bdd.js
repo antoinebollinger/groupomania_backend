4a57cdfe057fc56d03a47b0a5b69c2b46b8a5977
Antoine Bollinger <antoine.bollinger@gmail.com>
2021-04-22 07:46:39 +0200

    bdd improve

--- bdd/bdd.js
+++ bdd/bdd.js
@@ -2,6 +2,31 @@ const mysql = require('mysql');
 const queries = require('./bdd.json');
 require('dotenv').config();
 
+const connInit = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,   
+});
+
+connInit.connect();
+
+connInit.query("CREATE DATABASE IF NOT EXISTS "+process.env.DB_DATABASE+" /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */", function (err) {
+    if (err) throw err;
+    connInit.changeUser({database: process.env.DB_DATABASE}, function(err) {
+        if (err) throw err;
+        let i = 0;
+        for (const table in queries.tables) {
+            connInit.query(queries.tables[table], function(err, results) {
+                if (err) throw err;
+                i++;
+                if (i == Object.keys(queries.tables).length) {
+                    connInit.end();
+                }
+            });
+        }
+    });
+});
+
 // Création de la connection
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
@@ -12,20 +37,6 @@ const pool = mysql.createPool({
     reconnect: true,
     connectionLimit: 10,
     waitForConnections : false
-});
-
-pool.getConnection((err, connection) => {
-    connection.query("CREATE DATABASE IF NOT EXISTS "+process.env.DB_DATABASE+" /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */", function (err) {
-        if (err) throw err;
-        connection.changeUser({database: process.env.DB_DATABASE}, function(err) {
-            if (err) throw err;
-            for (const table in queries.tables) {
-                connection.query(queries.tables[table], function(err, results) {
-                    if (err) throw err;
-                });
-            }
-        });
-    })
 });
 
 pool.promise = (sql, sql_params, erreur) => {
